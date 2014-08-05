0554b109d0c415986982678fa3a56ddf930ee7c0
Ben Schmeckpeper <ben@aisle50.com>
2014-08-05 10:40:04 -0500

    UserMailer changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
-class UserMailer < ApplicationMailer
+class UserMailer < ActionMailer::Base
   default from: 'DICE! <dice@example.com>'
 
   def invite(invite)
     @invite = invite
 
-    mail(to: @invite.email, subject: "Dice? Invite from #{@invite.game.owner}")
+    mail(to: @invite.email, subject: "Dice? Invite from #{@invite.game.owner.name}")
   end
 end
