e042edc497e826806b672825a9d05d72e3aaad07
Ben Schmeckpeper <ben@aisle50.com>
2014-08-05 07:48:39 -0500

    Organize controller methods in GamesController.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,40 +1,10 @@
 class GamesController < ApplicationController
   before_action :authenticate_user!, except: :events
 
-  def create
-    @game = current_user.owned_games.create
-    handle = params[:handle]
-    handle = current_user.name if handle.blank?
-    @game.add_player(current_user, handle)
-
-    redirect_to @game
-  end
-
-  def start
-    @game = current_user.owned_games.find(params[:id])
-    player = @game.player_for(current_user)
-    handle = params[:handle]
-    handle = current_user.name if handle.blank?
-    player.update_attributes(handle: handle)
-    @game.start
-  end
-
-  def events
-    game = Game.find(params[:id])
-
-    render json: game.events(params[:prev_event])
-  end
-
-  def invite
-    game = current_user.owned_games.find(params[:id])
-
-    params[:emails].each do |email|
-      email = email.downcase
-      user = User.find_by_email(email)
-      invite = game.invites.create(user: user, email: email)
-      UserMailer.invite(invite).deliver
-    end
-  end
+  ##############################
+  ##  Controller Methods      ##
+  ##  (Sorted alphabetically) ##
+  ##############################
 
   def bid
     game = current_user.games.find(params[:id])
@@ -55,5 +25,40 @@ class GamesController < ApplicationController
   def bs
     game = current_user.games.find(params[:id])
     game.bs(current_user)
+  end
+
+  def create
+    @game = current_user.owned_games.create
+    handle = params[:handle]
+    handle = current_user.name if handle.blank?
+    @game.add_player(current_user, handle)
+
+    redirect_to @game
+  end
+
+  def events
+    game = Game.find(params[:id])
+
+    render json: game.events(params[:prev_event])
+  end
+
+  def invite
+    game = current_user.owned_games.find(params[:id])
+
+    params[:emails].each do |email|
+      email = email.downcase
+      user = User.find_by_email(email)
+      invite = game.invites.create(user: user, email: email)
+      UserMailer.invite(invite).deliver
+    end
+  end
+
+  def start
+    @game = current_user.owned_games.find(params[:id])
+    player = @game.player_for(current_user)
+    handle = params[:handle]
+    handle = current_user.name if handle.blank?
+    player.update_attributes(handle: handle)
+    @game.start
   end
 end
