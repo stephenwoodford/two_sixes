243cb0c13b2288655ef897c215cd34186fd2b52a
Ben Schmeckpeper <ben@aisle50.com>
2014-08-14 10:29:58 -0500

    Add Call#to_hash

--- app/models/call.rb
+++ app/models/call.rb
@@ -2,6 +2,16 @@ class Call < ActiveRecord::Base
   belongs_to :player
   belongs_to :round
 
+  def to_hash
+    ret = { bs: bs, seat: player.seat, sequenceNumber: sequence_number, legal: legal }
+    if bid
+      ret[:number] = bid.number
+      ret[:faceValue] = bid.face_value
+    end
+
+    ret
+  end
+
   def bid
     return nil if bs?
     Bid.new number, face_value
