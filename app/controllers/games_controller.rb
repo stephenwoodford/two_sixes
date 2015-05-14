986c5fb3664c86f32a9fdeb04ba206c2b5cbb3b3
Stephen Woodford <stephen.woodford@thefreeosk.com>
2015-05-14 10:53:45 -0500

    Comment out UserMailer for now, need to fix

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -28,7 +28,7 @@ class GamesController < ApplicationController
     invite = game.invites.create(user: user, email: email)
     if invite.valid?
       game.add_event(invite)
-      UserMailer.invite(invite).deliver
+      #UserMailer.invite(invite).deliver # TODO: Fix email on production
     else
       messages = invite.errors.messages
       if messages[:email] == ["is not an email"]
